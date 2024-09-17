7d56583ec2608061d1c2e0cabe238968c3fe9bc5
Texac56 <texax.vaic@gmail.com>
2024-09-17 15:04:22 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первое число: ");
+        int num = scanner.nextInt();
+        System.out.println("Введите второе число: ");
+        int num1 = scanner.nextInt();
+
+        int res;
+
+        System.out.print("Действие: ");
+        String action = scanner.nextLine();
+        action = scanner.nextLine();
+        switch (action) {
+            case "+":
+                res = num + num1;
+                System.out.println("Результат: " + res );
+                break;
+            case "-":
+                res = num - num1;
+                System.out.println("Результат: " + res );
+                break;
+            case "/":
+                res = num / num1;
+                System.out.println("Результат: " + res );
+                break;
+            case "*":
+                res = num * num1;
+                System.out.println("Результат:" + res );
+                break;
+            default:
+                System.out.println("Что-то не так");
+        }
+
+        }
+    }
